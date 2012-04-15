becdd97eb7c1dcb12e799bf8526ea2d340f25145
kitao <takashi.kitao@gmail.com>
2012-04-16 07:53:32 +0900

    Added tentative code

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -13,4 +13,23 @@ window.onload = function() {
   sprite.size(100, 100).image("http://placekitten.com/200/200").position(130, 30, 0).opacity(0.5).rotation(30, 30, 0);
 
   scene.appendChild(sprite);
+
+
+  /*function newTask() {
+    var pos = new DivSugar.Vector(0, 0, 0);
+    var spr = DivSugar.createSprite();
+
+    spr.size(100, 100).image("hoge").position(0, 0, 0);
+
+    var task = DivSugar.createTask(function() {
+      pos.x += 10;
+      spr.position(pos);
+
+      if (pos.x > 400) {
+        DivSugar.unregisterTask(this);
+      }
+    });
+
+    DivSugar.tasks.push(task);
+  }*/
 };
