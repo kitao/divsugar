6d2caa53b9287414f1f45e2c4a2d0a7064146e6b
kitao <takashi.kitao@gmail.com>
2012-04-18 00:04:38 +0900

    Made simpler

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -4,26 +4,22 @@ window.onload = function() {
   var sceneHolder = document.getElementById('sceneHolder');
 
   var scene = DivSugar.createScene();
-  scene.size(600, 400, 600, 400);
+  scene.size(600, 400, 300, 200).image('#0000ff').position(100, 100);
   sceneHolder.appendChild(scene);
 
-  scene.style.backgroundColor = "#0000ff";
-
   var sprite = DivSugar.createSprite();
-  sprite.size(300, 100).image("http://placekitten.com/200/200").position(130, 30, 0).opacity(0.5).rotation(30, 30, 0);
-
+  sprite.size(100, 100).image("http://placekitten.com/200/200").position(130, 30, 0).opacity(0.5).rotation(0, 0, 30);
   scene.appendChild(sprite);
 
   var task = DivSugar.createTask('hoge');
-  DivSugar.rootTask.appendChild(task);
-
-  var w = 100;
 
   task.onUpdate = function() {
-    sprite.size(w, w);
-    w += 1;
+    var size = sprite.size();
+    var rot = sprite.rotation();
 
-    if (w > 300) {
+    sprite.size(size.w + 1, size.h + 1).rotation(rot.x, rot.y + 1, rot.z);
+
+    if (size.w > 400) {
       this.destroy();
     }
   };
@@ -31,4 +27,6 @@ window.onload = function() {
   task.onDestroy = function() {
     sprite.parentNode.removeChild(sprite);
   };
+
+  DivSugar.rootTask.appendChild(task);
 };
