d39b383a2f43f34b601379e86c63181b5f76dc9b
Takashi Kitao <takashi.kitao@gmail.com>
2012-04-17 15:39:21 +0900

    Added Task-test code

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -10,26 +10,25 @@ window.onload = function() {
   scene.style.backgroundColor = "#0000ff";
 
   var sprite = DivSugar.createSprite();
-  sprite.size(100, 100).image("http://placekitten.com/200/200").position(130, 30, 0).opacity(0.5).rotation(30, 30, 0);
+  sprite.size(300, 100).image("http://placekitten.com/200/200").position(130, 30, 0).opacity(0.5).rotation(30, 30, 0);
 
   scene.appendChild(sprite);
 
+  var task = new DivSugar.Task('hoge');
+  DivSugar.rootTask.appendChild(task);
 
-  /*function newTask() {
-    var pos = new DivSugar.Vector(0, 0, 0);
-    var spr = DivSugar.createSprite();
+  var w = 100;
 
-    spr.size(100, 100).image("hoge").position(0, 0, 0);
+  task.onUpdate = function() {
+    sprite.size(w, w);
+    w += 1;
 
-    var task = DivSugar.createTask(function() {
-      pos.x += 10;
-      spr.position(pos);
+    if (w > 300) {
+      this.destroy();
+    }
+  };
 
-      if (pos.x > 400) {
-        DivSugar.unregisterTask(this);
-      }
-    });
-
-    DivSugar.tasks.push(task);
-  }*/
+  task.onDestroy = function() {
+    sprite.parentNode.removeChild(sprite);
+  };
 };
