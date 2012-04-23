11d0d9903d6113923ed3e2204d6110295c03df85
kitao <takashi.kitao@gmail.com>
2012-04-23 23:25:03 +0900

    Completed tests

--- test/QuaternionTest.js
+++ test/QuaternionTest.js
@@ -1,54 +1,92 @@
 module('Quaternion');
 
-    quat1 = new b9.Quaternion();
+test('constructor', function() {
+  var quat1 = new DivSugar.Quaternion();
+  strictEqual(quat1.x, 0);
+  strictEqual(quat1.y, 0);
+  strictEqual(quat1.z, 0);
+  strictEqual(quat1.w, 0);
 
-    quat2 = new b9.Quaternion(1, 2, 3, 4);
-    assertTrue(quat2.x === 1 && quat2.y === 2 && quat2.z === 3 && quat2.w === 4);
+  var quat2 = new DivSugar.Quaternion(1, 2, 3, 4);
+  strictEqual(quat2.x, 1);
+  strictEqual(quat2.y, 2);
+  strictEqual(quat2.z, 3);
+  strictEqual(quat2.w, 4);
 
-    quat3 = new b9.Quaternion(quat2);
-    assertTrue(quat3.x === 1 && quat3.y === 2 && quat3.z === 3 && quat3.w === 4);
+  var quat3 = new DivSugar.Quaternion(quat2);
+  strictEqual(quat3.x, 1);
+  strictEqual(quat3.y, 2);
+  strictEqual(quat3.z, 3);
+  strictEqual(quat3.w, 4);
+});
 
-    /* set */
-    assertEquals_object(new b9.Quaternion(3, 4, 5, 6), quat1.set(3, 4, 5, 6));
-    assertEquals_object(quat1, quat2.set(quat1));
+test('set', function() {
+  var quat1 = new DivSugar.Quaternion();
+  quat1.set(1, 2, 3, 4);
+  deepEqual(quat1, new DivSugar.Quaternion(1, 2, 3, 4));
 
-    /* fromMatrix3D */
-    assertEquals_object(new b9.Quaternion(0, 0, 0, 1), quat1.fromMatrix3D(DivSugar.Matrix.UNIT));
+  var quat2 = new DivSugar.Quaternion();
+  quat2.set(quat1);
+  deepEqual(quat2, new DivSugar.Quaternion(1, 2, 3, 4));
 
-    /* slerp */
-    for (ratio = 0; ratio <= 1; ratio += 0.5) {
-        quat1.fromMatrix3D(new DivSugar.Matrix(DivSugar.Matrix.UNIT));
-        quat2.fromMatrix3D((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateX_int(90));
-        quat3.fromMatrix3D((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateX_float(90 * ratio));
-        assertEquals_object(quat3, quat1.slerp(quat2, ratio));
+  ok(quat1.set(0, 0, 0, 0).set(quat2).set(1, 1, 1, 1));
+});
 
-        quat1.fromMatrix3D(new DivSugar.Matrix(DivSugar.Matrix.UNIT));
-        quat2.fromMatrix3D((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateY_int(90));
-        quat3.fromMatrix3D((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateY_float(90 * ratio));
-        assertEquals_object(quat3, quat1.slerp(quat2, ratio));
+test('fromMatrix', function() {
+  var quat1 = new DivSugar.Quaternion();
+  quat1.fromMatrix(DivSugar.Matrix.UNIT);
+  deepEqual(quat1, new DivSugar.Quaternion(0, 0, 0, 1));
 
-        quat1.fromMatrix3D(new DivSugar.Matrix(DivSugar.Matrix.UNIT));
-        quat2.fromMatrix3D((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateZ_int(90));
-        quat3.fromMatrix3D((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateZ_float(90 * ratio));
-        assertEquals_object(quat3, quat1.slerp(quat2, ratio));
-    }
+  ok(quat1.fromMatrix(DivSugar.Matrix.UNIT).fromMatrix(DivSugar.Matrix.UNIT));
+});
 
-    /* equals */
-    quat1.set(1, 2, 3, 4);
-    quat2.set(quat1);
-    assertTrue(quat1.equals(quat2));
+test('slerp', function() {
+  var quat1 = new DivSugar.Quaternion();
+  var quat2 = new DivSugar.Quaternion();
+  var quat3 = new DivSugar.Quaternion();
 
-    quat2.set(1.1, 2, 3, 4);
-    assertFalse(quat1.equals(quat2));
+  for (var ratio = 0; ratio <= 1; ratio += 0.1) {
+    quat1.fromMatrix(new DivSugar.Matrix(DivSugar.Matrix.UNIT));
+    quat2.fromMatrix((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateX(90));
+    quat3.fromMatrix((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateX(90 * ratio));
+    quat1.slerp(quat2, ratio);
+    nearlyEqual(quat1, quat3);
 
-    quat2.set(1, 2.1, 3, 4);
-    assertFalse(quat1.equals(quat2));
+    quat1.fromMatrix(new DivSugar.Matrix(DivSugar.Matrix.UNIT));
+    quat2.fromMatrix((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateY(90));
+    quat3.fromMatrix((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateY(90 * ratio));
+    quat1.slerp(quat2, ratio);
+    nearlyEqual(quat1, quat3);
 
-    quat2.set(1, 2, 3.1, 4);
-    assertFalse(quat1.equals(quat2));
+    quat1.fromMatrix(new DivSugar.Matrix(DivSugar.Matrix.UNIT));
+    quat2.fromMatrix((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateZ(90));
+    quat3.fromMatrix((new DivSugar.Matrix(DivSugar.Matrix.UNIT)).rotateZ(90 * ratio));
+    quat1.slerp(quat2, ratio);
+    nearlyEqual(quat1, quat3);
+  }
 
-    quat2.set(1, 2, 3, 4.1);
-    assertFalse(quat1.equals(quat2));
+  ok(quat1.slerp(quat2, 0).slerp(quat2, 0.5).slerp(quat2, 1).slerp(quat2, 0));
+});
 
-    /* toString */
-    assertEquals("(1, 2, 3, 4)", (new b9.Quaternion(1, 2, 3, 4)).toString());
+test('equal', function() {
+  var quat1 = new DivSugar.Quaternion(1, 2, 3, 4);
+  var quat2 = new DivSugar.Quaternion(quat1);
+  ok(quat1.equal(quat2));
+
+  quat2.set(1.1, 2, 3, 4);
+  ok(!quat1.equal(quat2));
+
+  quat2.set(1, 2.1, 3, 4);
+  ok(!quat1.equal(quat2));
+
+  quat2.set(1, 2, 3.1, 4);
+  ok(!quat1.equal(quat2));
+
+  quat2.set(1, 2, 3, 4.1);
+  ok(!quat1.equal(quat2));
+});
+
+test('toString', function() {
+  var quat1 = new DivSugar.Quaternion(1, 2, 3, 4);
+  strictEqual(quat1.toString(), '(1, 2, 3, 4)');
+});
