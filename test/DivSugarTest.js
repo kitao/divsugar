f89bcc4820488020eb16bb7436e0bdfbea70b989
kitao <takashi.kitao@gmail.com>
2012-05-01 14:25:09 +0900

    Modified the version test

--- test/DivSugarTest.js
+++ test/DivSugarTest.js
@@ -4,7 +4,7 @@
   module('DivSugar');
 
   test('constants', function() {
-    strictEqual(DivSugar.VERSION, '0.10');
+    strictEqual(typeof(DivSugar.VERSION), 'string');
     strictEqual(DivSugar.EPSILON, 0.0001);
     strictEqual(DivSugar.NUM_OF_DIGITS, 4);
     strictEqual(DivSugar.DEG_TO_RAD * 180, Math.PI);
