c7f8787f3238ed26d19f27a137e7a8792b770453
kitao <takashi.kitao@gmail.com>
2012-05-01 09:59:14 +0900

    Added check code

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -6,17 +6,37 @@ window.onload = function() {
   document.body.appendChild(scn);
 
   var spr = DivSugar.createSprite();
-  spr.setSize(100, 100).setImage('http://placekitten.com/200/200').setPosition(330, 130, 0).setOpacity(0.5).rotateZ(30);
+  spr.setSize(200, 200).setImage('http://placekitten.com/200/200').setPosition(330, 130, 0).setOpacity(0.5).rotate(0, 0, 30);
+  spr.setImageClip(0, 0, 1, 1);
   scn.appendChild(spr);
+
+  DivSugar.addCSSAnimation('testAnim', {
+    from: {
+      imageClip: [0,0,1,1],
+      visible: true,
+      clip: true,
+      opacity: 1
+    },
+    '100%': {
+      size: [300, 300],
+      imageClip: [0.5,0.5,1,1],
+      opacity: 0.5,
+      iamge: 'http://placekitten.com/300/300',
+      visible: false,
+      rotate: [0, 40, 0]
+    }
+  });
 
   var task = DivSugar.createTask('hoge');
   var vec = new DivSugar.Vector();
+
+  //spr.playCSSAnimation('testAnim', 2, 'linear', 0, 'infinite', 'alternate');
 
   task.onUpdate = function(elapsedTime) {
     var width = spr.getWidth();
     var height = spr.getHeight();
 
-    spr.setSize(width + elapsedTime * 0.05, height + elapsedTime * 0.05).rotateX(elapsedTime * 0.05);
+    spr.setSize(width + elapsedTime * 0.05, height + elapsedTime * 0.05).rotate(elapsedTime * 0.05, 0, 0);
     if (width > 400) {
       this.destroy();
     }
