c09f259c5c34708a950c9212eaadbf392b9e1e20
kitao <takashi.kitao@gmail.com>
2012-05-03 13:35:09 +0900

    Updated

--- examples/02.TaskAnimation/main.js
+++ examples/02.TaskAnimation/main.js
@@ -1,13 +1,39 @@
 window.onload = function() {
   'use strict';
 
-  // create a scene which is a rendering target of nodes
+  // create a scene
   var scn = DivSugar.createScene();
   scn.setSize(800, 600).setImage('#000080');
   document.body.appendChild(scn);
 
-  // maximize the scene size as possible
-  function resize() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); }
-  window.addEventListener('resize', resize, false);
-  resize();
+  window.onresize = function() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); };
+  window.onresize();
+
+  // define an animation class with a task
+  function Hoge(parentNode, x, y, z) {
+    this.constructor.uber.constructor(); // call parent class's constructor
+
+    this.node1 = DivSugar.createNode();
+    this.node1.setPosition(x, y, z);
+    parentNode.appendChild(this.node1);
+
+    this.node1.setSize(200, 200).setImage('http://placekitten.com/200/200');
+
+    DivSugar.rootTask.appendChild(this);
+  }
+
+  DivSugar.inherit(Hoge, DivSugar.Task);
+
+  Hoge.prototype.onUpdate = function(elapsedTime) {
+    this.node1.rotate(0, elapsedTime * 0.1, 0);
+  };
+
+  Hoge.prototype.onDestroy = function() {
+    this.node1.parentNode.removeChild(this.node1);
+  };
+
+  // create instances
+  var dummy;
+  dummy = new Hoge(scn, 100, 100, 0);
+  dummy = new Hoge(scn, 400, 300, -100);
 };
--- examples/03.CSSAnimation/main.js
+++ examples/03.CSSAnimation/main.js
@@ -1,13 +1,68 @@
 window.onload = function() {
   'use strict';
 
-  // create a scene which is a rendering target of nodes
+  // TBD
+
+  // create a scene
   var scn = DivSugar.createScene();
   scn.setSize(800, 600).setImage('#000080');
   document.body.appendChild(scn);
 
-  // maximize the scene size as possible
+  // maximize the scene size
   function resize() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); }
   window.addEventListener('resize', resize, false);
   resize();
 };
+
+/*
+window.onload = function() {
+  'use strict';
+
+  var scn = DivSugar.createScene();
+  scn.setSize(600, 400).setPosition(200, 30).setImage('#0000ff');
+  document.body.appendChild(scn);
+
+  var node = DivSugar.createNode();
+  node.setSize(200, 200).setImage('http://placekitten.com/200/200').setPosition(330, 130, 0).setOpacity(0.5).rotate(0, 0, 30);
+  node.setImageClip(0, 0, 1, 1);
+  scn.rootNode.appendChild(node);
+
+  DivSugar.addCSSAnimation('testAnim', {
+    from: {
+      imageClip: [0,0,1,1],
+      visible: true,
+      clip: true,
+      opacity: 1
+    },
+    '100%': {
+      size: [300, 300],
+      imageClip: [0.5,0.5,1,1],
+      opacity: 0.5,
+      iamge: 'http://placekitten.com/300/300',
+      visible: false,
+      rotate: [0, 40, 0]
+    }
+  });
+
+  var task = DivSugar.createTask('hoge');
+  var vec = new DivSugar.Vector();
+
+  //node.playCSSAnimation('testAnim', 2, 'linear', 0, 'infinite', 'alternate');
+
+  task.onUpdate = function(elapsedTime) {
+    var width = node.getWidth();
+    var height = node.getHeight();
+
+    node.setSize(width + elapsedTime * 0.05, height + elapsedTime * 0.05).rotate(elapsedTime * 0.05, 0, 0);
+    if (width > 400) {
+      this.destroy();
+    }
+  };
+
+  task.onDestroy = function() {
+    node.parentNode.removeChild(node);
+  };
+
+  DivSugar.rootTask.appendChild(task);
+};
+*/
