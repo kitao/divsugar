8f7a8e8079a96d9038841225c48e0ead32f4c033
kitao <takashi.kitao@gmail.com>
2012-05-05 20:00:35 +0900

    Updated

--- examples/03.CSSAnimation/main.js
+++ examples/03.CSSAnimation/main.js
@@ -9,58 +9,29 @@ window.onload = function() {
   window.onresize = function() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); };
   window.onresize();
 
-  // TBD
-};
-
-/*
-window.onload = function() {
-  'use strict';
-
-  var scn = DivSugar.createScene();
-  scn.setSize(600, 400).setPosition(200, 30).setImage('#0000ff');
-  document.body.appendChild(scn);
-
-  var node = DivSugar.createNode();
-  node.setSize(200, 200).setImage('http://placekitten.com/200/200').setPosition(330, 130, 0).setOpacity(0.5).rotate(0, 0, 30);
-  node.setImageClip(0, 0, 1, 1);
-  scn.rootNode.appendChild(node);
-
-  DivSugar.addCSSAnimation('testAnim', {
+  // register CSS animations dynamically
+  DivSugar.addCSSAnimation('scroll', {
     from: {
-      imageClip: [0,0,1,1],
-      visible: true,
-      clip: true,
-      opacity: 1
+      opacity: 0,
+      rotate: [90, 0, 0],
+      scale: [3, 2, 1]
     },
-    '100%': {
-      size: [300, 300],
-      imageClip: [0.5,0.5,1,1],
-      opacity: 0.5,
-      iamge: 'http://placekitten.com/300/300',
-      visible: false,
-      rotate: [0, 40, 0]
+    to: {
+      opacity: 1,
+      rotate: [0, 0, 0],
+      scale: [1, 1, 1]
     }
   });
 
-  var task = DivSugar.createTask('hoge');
-  var vec = new DivSugar.Vector();
+  //
+  var water1 = DivSugar.createNode();
+  water1.setSize(300, 300).setImage('../assets/water.jpg').setPosition(400, 300, 0);
+  scn.appendChild(water1);
 
-  //node.playCSSAnimation('testAnim', 2, 'linear', 0, 'infinite', 'alternate');
+  var water2 = DivSugar.createNode();
+//  water2.setSize(300, 300).setImage('../assets/water.jpg').setPosition(400, 300, 0);
+  scn.appendChild(water2);
 
-  task.onUpdate = function(elapsedTime) {
-    var width = node.getWidth();
-    var height = node.getHeight();
-
-    node.setSize(width + elapsedTime * 0.05, height + elapsedTime * 0.05).rotate(elapsedTime * 0.05, 0, 0);
-    if (width > 400) {
-      this.destroy();
-    }
-  };
-
-  task.onDestroy = function() {
-    node.parentNode.removeChild(node);
-  };
-
-  DivSugar.rootTask.appendChild(task);
+  water1.playCSSAnimation('scroll', 1, 'linear', 0, 'infinite');
+  //water2.playCSSAnimation('scroll', 13, 'linear', 0, 'infinite');
 };
-*/
