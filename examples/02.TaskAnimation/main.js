31b829e240ee95d3f29a21a6e1b5e66a686228bf
kitao <takashi.kitao@gmail.com>
2012-05-05 15:11:27 +0900

    Completed implementation

--- examples/02.TaskAnimation/main.js
+++ examples/02.TaskAnimation/main.js
@@ -9,39 +9,52 @@ window.onload = function() {
   window.onresize = function() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); };
   window.onresize();
 
-  // define an animation class with a task
-  function Hoge(parentNode, x, y, z) {
-    this.constructor.uber.constructor(); // call parent class's constructor
+  // define an animation class with the task system
+  function Coin() {
+    // call parent class's constructor
+    this.constructor.uber.constructor();
 
-    this.node = DivSugar.createNode();
-    this.node.setPosition(x, y, z);
-    parentNode.appendChild(this.node);
+    this.pos = new DivSugar.Vector(Math.random() * 800, 900, Math.random() * 800 - 1000);
+    this.vel = new DivSugar.Vector(Math.random() * 6 - 3, Math.random() * 8 - 20, Math.random() * 6 - 3);
+    this.rot = new DivSugar.Vector(Math.random() * 0.1, Math.random() * 0.1, Math.random() * 0.1);
 
-    this.node1 = DivSugar.createNode();
-    this.node1.setSize(200, 200).setImage('../assets/coin.png').setImageClip(0, 0, 0.5, 1).setBackface(false);
-    this.node1.setPosition(-100, -100, 0);
-    this.node.appendChild(this.node1);
+    this.center = DivSugar.createNode();
+    this.center.setPosition(this.pos).rotate(Math.random() * 360, Math.random() * 360, Math.random() * 360);
+    scn.appendChild(this.center);
 
-    this.node2 = DivSugar.createNode();
-    this.node2.setSize(200, 200).setImage('../assets/coin.png').setImageClip(0.5, 0, 1, 1);
-    this.node2.setPosition(100, -100, 0).rotate(0, 180, 0).setBackface(false);
-    this.node.appendChild(this.node2);
+    this.front = DivSugar.createNode();
+    this.front.setSize(200, 200).setImage('../assets/coin.png').setImageClip(0, 0, 0.5, 1).setBackface(false).setPosition(-100, -100, 0);
+    this.center.appendChild(this.front);
 
+    this.back = DivSugar.createNode();
+    this.back.setSize(200, 200).setImage('../assets/coin.png').setImageClip(0.5, 0, 1, 1).setBackface(false).setPosition(100, -100, 0).rotate(0, 180, 0);
+    this.center.appendChild(this.back);
+
+    // register this task to the rootTask to be updated in every frame
     DivSugar.rootTask.appendChild(this);
   }
 
-  DivSugar.inherit(Hoge, DivSugar.Task);
+  DivSugar.inherit(Coin, DivSugar.Task);
 
-  Hoge.prototype.onUpdate = function(elapsedTime) {
-    this.node.rotate(0, elapsedTime * 0.15, elapsedTime * 0.1);
+  Coin.prototype.onUpdate = function(elapsedTime) {
+    this.vel.y += elapsedTime * 0.01;
+    this.pos.add(this.vel);
+    this.center.setPosition(this.pos).rotate(elapsedTime * this.rot.x, elapsedTime * this.rot.y, elapsedTime * this.rot.z);
+
+    // when fall enough, destroy this task and create an another task
+    if (this.pos.y > 1000) {
+      this.destroy();
+      var dummy = new Coin();
+    }
   };
 
-  Hoge.prototype.onDestroy = function() {
-    this.node.parentNode.removeChild(this.node1);
+  Coin.prototype.onDestroy = function() {
+    scn.removeChild(this.center);
   };
 
-  // create instances
-  var dummy;
-  dummy = new Hoge(scn, 100, 300, 400);
-  dummy = new Hoge(scn, 400, 300, -100);
+  // create instances of the animation class
+  var dummy; // for avoiding jslint warnings
+  for (var i = 0; i < 30; i++) {
+    dummy = new Coin();
+  }
 };
