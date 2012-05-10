5c6b1208830d4869cdd55dfc398840d5f98abe3d
kitao <takashi.kitao@gmail.com>
2012-05-11 00:17:32 +0900

    Implementing

--- examples/03.TweenAnimation/main.js
+++ examples/03.TweenAnimation/main.js
@@ -8,4 +8,15 @@ window.onload = function() {
 
   window.onresize = function() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); };
   window.onresize();
+
+  //
+  var node = DivSugar.createNode();
+  node.setSize(100, 100).setImage('#ff0000');
+  scn.appendChild(node);
+
+  node.playAnimation([
+    ['to', { size: [100, 100], position: [400, 200, 0], rotate: [0, 0, 90], scale: [2, 2, 1] }, 1000],
+    ['to', { size: [50, 200], position: [0, 0, 0], rotate: [0, 0, -90], scale: [0.5, 0.5, 1] }, 1000],
+    ['repeat']
+  ]);
 };
