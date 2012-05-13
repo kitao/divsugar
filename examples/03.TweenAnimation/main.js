24c9fe363a3bea8e202710a6dc75ea9a7866a060
kitao <takashi.kitao@gmail.com>
2012-05-14 00:43:36 +0900

    Implementing

--- examples/03.TweenAnimation/main.js
+++ examples/03.TweenAnimation/main.js
@@ -2,33 +2,44 @@ window.onload = function() {
   'use strict';
 
   // create a scene
-  var scn = DivSugar.createScene()
-    .setSize(800, 600)
-    .setImage('../assets/sunset.jpg')
-    .appendTo(document.body);
+  var scn = DivSugar.createScene().setSize(800, 600).setImage('#000030').appendTo(document.body);
 
   // maximize the scene size as possible
-  window.onresize = function() {
-    scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain');
-  };
+  window.onresize = function() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); };
   window.onresize();
 
-  //
-  var water1 = DivSugar.createNode()
-    .setSize(400, 300) .setPosition(0, 0, 0) .setImage('../assets/water.jpg') .setImageClip(0, 0, 0.5, 0.5) .appendTo(scn);
+  // create the center node
+  var center = DivSugar.createNode().setPosition(400, 300, 0).appendTo(scn);
+  center.playAnimation([['to', { rotate: [360, 0, -10] }, 10000], ['repeat']]);
 
-  var water2 = DivSugar.createNode()
-    .setSize(400, 300) .setPosition(0, 0, 100) .setImage('../assets/water.jpg') .setImageClip(0, 0, 0.5, 0.5) .setOpacity(0.5) .appendTo(scn);
+  // create banner nodes
+  var colors = ['#ffff80', '#ff80ff', '#80ffff', '#ff8080', '#80ff80', '#8080ff'];
 
-  water1.playAnimation([
-    ['to', { imageClip: [0, 0, 0.5, 0.5] }, 0 ],
-    ['to', { imageClip: [0.5, 0.5, 1, 1] }, 10000 ],
-    ['repeat']
-  ]);
+  function createBanner(waitTime, x, y, z, color) {
+    var banner = DivSugar.createNode().setSize(1200, 100).setOpacity(0).appendTo(center);
+    banner.setBackface(false);
+    banner.rotate(Math.random() * 360, 0, 0);
 
-  water2.playAnimation([
-    ['to', { imageClip: [0, 0, 0.5, 0.5] }, 0 ],
-    ['to', { imageClip: [0.5, 0.5, 1, 1] }, 15000 ],
-    ['repeat']
-  ]);
+    banner.innerHTML = '<h1 style="color:' + color + '; font-size:50px">DivSuar, a CSS-based graphics library</h1>';
+
+    var anim1 = [
+      ['to', { position: [x + 400, y, z], opacity: 0 }],
+      ['to', { position: [x + 200, y, z], opacity: 0.8 }, 5000 / 4],
+      ['to', { position: [x - 200, y, z], opacity: 0.8 }, 5000 / 2],
+      ['to', { position: [x - 400, y, z], opacity: 0 }, 5000 / 4],
+      ['repeat']
+    ];
+    var anim2 = [['wait', waitTime], ['play', anim1]];
+
+    banner.playAnimation(anim2);
+  }
+
+  for (var i = 0; i < 60; i++) {
+    var time = Math.random() * 5000;
+    var x = Math.random() * 1600 - 800;
+    var y = Math.random() * 300 - 300;
+    var z = Math.random() * 800 - 400;
+    var color = colors[Math.floor(Math.random() * 6)];
+    createBanner(time, x, y, z, color);
+  }
 };
