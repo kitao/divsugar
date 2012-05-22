11b330594551fa8c262ea825f49e983d0b392a36
kitao <takashi.kitao@gmail.com>
2012-05-23 07:29:21 +0900

    Added the API reference

---
+++ docs/api/api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DivSugar",
+        "DivSugar.Ease",
+        "DivSugar.Matrix",
+        "DivSugar.Node",
+        "DivSugar.Quaternion",
+        "DivSugar.Scene",
+        "DivSugar.Task",
+        "DivSugar.Vector"
+    ],
+    "modules": [
+        "DivSugar"
+    ],
+    "allModules": [
+        {
+            "displayName": "DivSugar",
+            "name": "DivSugar"
+        }
+    ]
+} };
+});
