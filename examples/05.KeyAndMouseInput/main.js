5da0d116238dda687a3b95eff0f26ee7b560dab4
Takashi Kitao <takashi.kitao@gmail.com>
2012-07-17 16:35:49 +0900

    Implementing

--- examples/05.KeyAndMouseInput/main.js
+++ examples/05.KeyAndMouseInput/main.js
@@ -2,9 +2,15 @@ window.onload = function() {
   'use strict';
 
   // create a scene
-  var scn = new DivSugar.Scene().setSize(800, 600).setImage('#d0d0d0').appendTo(document.body);
+  var scn = new DivSugar.Scene().setSize(800, 600).setImage('#004000').appendTo(document.body);
 
   // maximize the scene size
   window.onresize = function() { scn.adjustLayout(window.innerWidth, window.innerHeight, 'contain'); };
   window.onresize();
+
+  // TBD
+  var task = new DivSugar.Task().appendTo(DivSugar.rootTask);
+  task.onUpdate = function() {
+    //console.log(DivSugar.getFrameCount());
+  };
 };
